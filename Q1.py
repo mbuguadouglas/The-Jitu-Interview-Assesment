6666ea782d44f2410a685f851c69e74baaa0ddb6
Mbugua Douglas <mbuguadouglas17@gmail.com>
2024-02-13 14:25:39 +0300

    solved question 1 and 3

--- Q1.py
+++ Q1.py
@@ -4,3 +4,25 @@
 # multiples of 5, print "Buzz"; and for numbers that are multiples of both 3 and 5, print
 # "FizzBuzz"
 
+# initialize your variable 
+i=1
+
+# create the loop and set its range from 1 to 100
+for i in range(1,101):
+	#see whether the variable is a multiple of 3
+	if (i%3==0):
+		print(f'{i}: Fizz')
+	# see whether the variable is a multiple of 5
+	elif (i%5==0):
+		print(f'{i}: Buzz')
+	#see whether the variable is a multiple of both 3 AND 5
+	elif (i%3==0) and (i%5==0):
+		print(f'{i}: FizzBuzz')
+	#check if variable is not a multple of both
+	# else:
+		# print(f'The value of i is: {i}, which is neither a multiple of 3 or 5')
+	#increment the variable to move onto next digit
+	i=i+1
+
+
+
--- Q3.py
+++ Q3.py
@@ -5,3 +5,24 @@
 
 # 8=> returns true
 # 6=> returns false
+
+# explin to the user what the program does and take their input
+print('The below program takes an input integer and returns true if it is a power of 2, else it returns false...')
+my_num= int(input('Input a number to work with here: '))
+
+# check whether the number is divisible by 2
+if (my_num%2 == 0):
+	print('True')
+# check whether the number is not divisible by 2
+elif (my_num%2 != 0):
+	print('False')
+# what to do incase of wrong input
+else:
+	print('Please input a valid number!')
+
+
+
+
+
+
+
