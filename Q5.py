0c9ab95ae6d79bf7192a2711df20c9f98d06ecc6
Mbugua Douglas <mbuguadouglas17@gmail.com>
2024-02-14 13:03:23 +0300

    solved question 5 and 6

--- Q5.py
+++ Q5.py
@@ -1,10 +1,31 @@
 # Question 5: Reverse Integer
 
-# Write a program that takes an integer as input and returns an integer with reversed digit
-# ordering.
+# Write a program that takes an integer as input and returns an integer 
+# with reversed digit ordering.
 
 # Examples:
 # For input 500, the program should return 5.
 # For input -56, the program should return -65.
 # For input -90, the program should return -9.
 # For input 91, the program should return 19.
+
+#initialize your variable to contain the inputed integer
+my_int = int(input('Input your number here: '))
+
+# begin an if statement to check whether inputed value is of int type
+if (type(my_int) == int):
+	reversed_str = str(my_int)[::-1]
+	print(reversed_str)
+
+# if not an integer type, return an error message
+elif (type(my_int) != int):
+	print('Inputed value is not of integer type')
+	
+# if no value was inputted, return an error
+else:
+	print('Kindly input a value')
+
+
+
+
+
--- Q6.py
+++ Q6.py
@@ -2,3 +2,22 @@
 
 # Write a program that counts the number of vowels in a sentence.
 # eg " Hello World " => returns 2
+
+
+print('Below is a program that takes a sentence of word as input and return the number of vowels it contains.')
+snippet = str(input('Input your senstence/word here: ')) 	#initialize a string variable
+
+my_vowels= ['a','e','i','o','u'] 	#declare a list containing all the vowels
+count = 0 							#initialize count to begin at 0
+
+# begin a for loop to iterate over the snippet variable
+for i in range(len(snippet)):
+	if snippet[i] in my_vowels: 	#use in operator to determine whether vowel values are in the snippet sequence
+		count+=1 					#if vowel found increment the value of count
+
+print(f'Your sentence has {count} vowels')  	#print out the number of vowels found
+	
+	
+
+
+
