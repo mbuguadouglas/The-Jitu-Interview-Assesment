872445016a46946d54b1c5bfe9a95f9dba8db601
Mbugua Douglas <mbuguadouglas17@gmail.com>
2024-02-14 13:16:16 +0300

    code review

--- Q2.py
+++ Q2.py
@@ -9,13 +9,11 @@ num1=0 				#initialize num1 as 0
 num2=1 				#initialize num2 as 1
 next_term = None 	#create an empty variable to hold value of th next term
 my_limit=100 		#set limit of series to 100
-count = 0
 
 print('The Fibonacci Series: ')
 #define a while loop that stops when the declared limit is reached
 while (num1 <= my_limit):
-	print(f'{num1}') 			#prints the firs term of the series
-	next_term = num1 + num2 	#palce the sum of num1 and num2 into the empty variable next_term
+	print(f'{num1}') 			#prints the first term of the series
+	next_term = num1 + num2 	#place the sum of num1 and num2 into the empty variable next_term
 	num1=num2 					#re-initialize value of num2 into num1
 	num2=next_term   			#re-initialize value of next_term into num2
-	count+=1 					#increment the count by one
